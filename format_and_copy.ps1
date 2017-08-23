02fc971532b47059672a97502dd3a213ffa6a2e3
Stephen <stephen@sheckler.info>
2017-08-23 18:18:10 -0500

    v0.3

--- format_and_copy.ps1
+++ format_and_copy.ps1
@@ -1,21 +1,21 @@
 ﻿# This script formats all removable drives attached to a system and 
-# then copies a designated directory or file to the newly formatted drives
+# then copies the contents of a designated directory to the newly formatted drives
 
-# Version 0.2
+# Version 0.3
 
 # Written by Stephen Heckler
 
 # Variables
-$disk_label = "TEST"
-$source = "C:\Users\Stephen\Desktop\DVL.iso"
+$disk_label = ""
+$source = ""
 
-# Enumerates the USB drives on the system and gets their root
+# Enumerates the removable drives attached to the system and gets their root
 $drives = Get-WMIObject win32_volume | ? { $_.DriveType -eq 2 } | % { Get-PSDrive $_.DriveLetter[0] } | Format-List Root | Out-String
 
 # Removes the title of the line
 $drives = $drives -replace 'Root : ','' 
 
-# Format list of drives
+# Formats a list of drives
 $list_drives = (-split $drives) -join " "
 
 # Removes the ":\" after the drive letter
@@ -29,6 +29,10 @@ $copy_drives = -split $copy_drives
 
 # Places each drive letter on the same line
 $format_drives = (-split $drives) -join ""
+
+# Print number of connected drives
+$numberofdrives = $format_drives.Length
+echo "There will be $numberofdrives formatted"
 
 # Status message
 echo "These are the drives that will be formatted:"
@@ -48,7 +52,7 @@ $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no) 
 
 # Defines choice menu
-$result = $host.ui.PromptForChoice("Format Drives","Do you want to format these drives?", $options, 1) #default option is No
+$result = $host.ui.PromptForChoice("Format Drives","Do you want to format these drives?", $options, 1) # Default option is No
 
 switch ($result)
     {
@@ -56,21 +60,19 @@ switch ($result)
             # Status Message
             echo "`nBeginning formatting"
 
-            # Formats the drives with the desired label
-            Format-Volume -DriveLetter $format_drives -FileSystem FAT32 -NewFileSystemLabel $disk_label | Out-Null
+            # Formats the drives with the desired label. Pauses on any error.
+            try {
+                 Format-Volume -DriveLetter $format_drives -FileSystem FAT32 -NewFileSystemLabel $disk_label | Out-Null
+            }
+            catch {
+                pause
+            }
 
             # Status message
             echo "Formatting complete`n"
 
             # Status message
             echo "Beginning file copy`n"
-
-            # Copies files from the source to the drives in sequence
-            #foreach ($drive in $copy_drives) {
-            #    echo "Starting copy to $($drive):\"
-            #    robocopy $source "$($drive):\" /e /eta /mt:2
-            #    echo "Done copying to $($drive):\"
-            #    }
             
             # Creates a workflow to copy files from the source to the drives in parellel
             workflow parellelcopy {
@@ -80,11 +82,12 @@ switch ($result)
 
                 foreach -parallel ($drive in $copy_drives) {
                     echo "Starting copy to $($drive):\"
-                    robocopy $source "$($drive):\" /e /eta /fft
-                    #Copy-Item $source -Destination "$($drive):\" -Recurse
+                    robocopy $source "$($drive):\" /e /eta /fft # Robocopy will print status messages for every file copied
+                    #Copy-Item $source -Destination "$($drive):\" -Recurse # Copy-Item prints no output
                     echo "Done copying to $($drive):\"
                 }
             }
+
             # Runs the above workflow, passing outside variables into the workflow
             parellelcopy -copy_drives $copy_drives -source $source
 
@@ -95,9 +98,7 @@ switch ($result)
 
         1 {
             # Goodbye.
-            echo "Goodbye."
-            pause
-            
+            echo "Goodbye."          
         }
     }
 
